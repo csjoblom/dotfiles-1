f7aa35fa76156f12d2bc21f990d8491f408d9118
Derek Leverenz <derek.leverenz@gmail.com>
2013-05-28 14:24:53 -0700

    made local aliases

--- .zsh/aliases.sh
+++ .zsh/aliases.sh
@@ -8,33 +8,3 @@ alias vim='mvim -v'
 alias tmux='tmux -2'
 
 alias rmpyc='find . -name "*.pyc" -exec rm -rf {} \;'
-
-
-# work (blueprint) aliases:
-export BLUEPRINT_HOME="$HOME/Workspace/bph_api/"
-alias pg_start="pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"
-alias pg_stop="pg_ctl -D /usr/local/var/postgres stop"
-alias pg_status="pg_ctl -D /usr/local/var/postgres status"
-
-function start_blueprint() {
-    workon blueprint
-    cd $BLUEPRINT_HOME
-
-    # aliases:
-    alias br='./manage.py build_responses'
-    alias manage="./manage.py"
-    alias nuke="./manage.py nuke"
-    alias sm="./manage.py schemamigration --auto api"
-    alias mm="./manage.py migrate api --merge"
-    alias rms="rm -rf bph/static"
-    alias tf="./manage.py test front"
-    alias ta="./manage.py test api"
-
-
-    pg_status
-    [[ 3 == $? ]] && pg_start
-    return 0
-}
-
-alias bp="start_blueprint"
-
