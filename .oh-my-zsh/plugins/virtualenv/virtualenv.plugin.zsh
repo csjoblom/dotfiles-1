75693a04a6b51d3725dbcee0752de38f1ddb4f00
Derek Leverenz <derek.leverenz@gmail.com>
2013-06-11 13:33:11 -0700

    added updated oh-my-zsh stuff

--- .oh-my-zsh/plugins/virtualenv/virtualenv.plugin.zsh
+++ .oh-my-zsh/plugins/virtualenv/virtualenv.plugin.zsh
@@ -1,3 +1,9 @@
-# Prevent virtualenv from prepending (virtualenv name) to your prompt
-# Use virtualenv_prompt_info to add the env to your prompt.
+function virtualenv_prompt_info(){
+  local virtualenv_path="$VIRTUAL_ENV"
+  if [[ -n $virtualenv_path ]]; then
+    local virtualenv_name=`basename $virtualenv_path`
+    printf "%s[%s] " "%{${fg[yellow]}%}" $virtualenv_name
+  fi
+}
+
 export VIRTUAL_ENV_DISABLE_PROMPT=1
